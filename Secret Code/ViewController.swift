d3d58a546d7ef5426da8ebfb4beb1da0422d4796
michael Rohner <michael@michaels-MacBook-Pro-3.local>
2015-02-16 19:54:16 -0800

    Initial Commit

---
+++ Secret Code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Secret Code
+//
+//  Created by michael Rohner on 2/16/15.
+//  Copyright (c) 2015 Michael Rohner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
